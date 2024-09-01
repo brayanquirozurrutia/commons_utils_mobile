91580da8a2fd770419152e1d107b8320c537aa13
BrayanQuirozUrrutia <brayanquirozurrutia@gmail.com>
2024-09-01 17:28:01 -0400

    :construction: FEAT: Add endpoint to compare products

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './screens/Home/Home';
 import Ingredients from "./screens/Ingredients/Ingredients";
+import Products from "./screens/Products/Products";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +13,7 @@ export default function App() {
           <Stack.Navigator>
             <Stack.Screen name="Home" component={Home} />
             <Stack.Screen name="Ingredients" component={Ingredients} />
+            <Stack.Screen name="Products" component={Products} />
           </Stack.Navigator>
       </NavigationContainer>
   );
